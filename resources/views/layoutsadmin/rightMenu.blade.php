8b22ce42aec87e020a7885809e41521be7e418f3
shaynjas14 <86900110+shaynjas14@users.noreply.github.com>
2021-07-08 11:15:08 +0800

    fix right sidebar menu
    -add images
    -and write description

--- resources/views/layoutsadmin/rightMenu.blade.php
+++ resources/views/layoutsadmin/rightMenu.blade.php
@@ -2,8 +2,12 @@
  <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
     <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
+      <h5>MediaOne PH</h5>
+      <p>Mediaone is a team of dedicated web developers and web designers based in Davao City,
+       Philippines who came from a diversed media and interactive backgrounds.
+        We pride ourselves on having the skills to integrate cross Disciplinary experience into each project, 
+        taking advantage of our thirst for new and innovative ideas 
+        in order to bring out a wide variety of business solutions no matter what it takes to get there.</p>
     </div>
   </aside>
   <!-- /.control-sidebar -->
