7e087b6209c5b49ab1d05707792381f66f84b06b
shaynjas14 <86900110+shaynjas14@users.noreply.github.com>
2021-07-08 11:23:10 +0800

    Update leftMenu.blade.php

--- resources/views/layoutsadmin/leftMenu.blade.php
+++ resources/views/layoutsadmin/leftMenu.blade.php
@@ -88,8 +88,30 @@
 
               </li>
         
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
              
-          </li>      
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-sign-out-alt "></i>
+                  <p>
+                   Logout
+                  </p>
+                </a>
+              </li>    
 
 
           
