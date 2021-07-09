144d35db95cbb2a1d446ddef603e7beff85bf90d
shaynjas14 <86900110+shaynjas14@users.noreply.github.com>
2021-07-09 19:16:25 +0800

    Update assignvet.blade.php

--- resources/views/veterinary/assignvet.blade.php
+++ resources/views/veterinary/assignvet.blade.php
@@ -26,7 +26,7 @@
 <!-- Default box -->
 <div class="card">
     <div class="card-header">
-      <h3 class="card-title">Assign Veterinary /</h3>
+      <h3 class="card-title">Assign Veterinary /Clinic</h3>
   
       <div class="card-tools">
         <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
