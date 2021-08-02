9dc56ab9415586a8a7e872bea63f60ada784d494
unknown <vlagria3@gmail.com>
2021-08-02 17:39:23 +0800

    insert array useraccount db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,26 +13,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('user_accounts')->insert([
-            'user_name'=>'Dr.Vet12',
-            'user_password'=>'drvet123',
-            'user_mobile'=>'09887872122',
-            'user_email'=>'drvet12@gmail.com',
-            'userType_id'=>'2',
-        ]);
-        DB::table('user_accounts')->insert([
-            'user_name'=>'admin21',
-            'user_password'=>'admin12345',
-            'user_mobile'=>'0988773324',
-            'user_email'=>'admin21@yahoo.com',
-            'userType_id'=>'1',
-        ]);
-        DB::table('user_accounts')->insert([
-            'user_name'=>'user21',
-            'user_password'=>'user21345',
-            'user_mobile'=>'097512121121',
-            'user_email'=>'user12@gmail.com',
-            'userType_id'=>'2',
-        ]);
+     
+
+        DB::table('user_accounts')->insert(array(
+            array('user_name'=>'Dr.Vet12',
+                'user_password'=>'drvet123',
+                'user_mobile'=>'09887872122',
+                'user_email'=>'drvet12@gmail.com',
+                'userType_id'=>'2'),
+            array('user_name'=>'admin21',
+                'user_password'=>'admin12345',
+                'user_mobile'=>'0988773324',
+                'user_email'=>'admin21@yahoo.com',
+                'userType_id'=>'1'),
+            array('user_name'=>'user21',
+                'user_password'=>'user21345',
+                'user_mobile'=>'097512121121',
+                'user_email'=>'user12@gmail.com',
+                'userType_id'=>'3'),
+        ));
     }
 }
