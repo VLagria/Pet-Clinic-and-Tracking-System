6d58393fa9993455fae02f03f9e98b5e16105e01
shaynjas14 <86900110+shaynjas14@users.noreply.github.com>
2021-08-11 00:11:14 +0800

    Update CustProfileController.php

--- app/Http/Controllers/CustProfileController.php
+++ app/Http/Controllers/CustProfileController.php
@@ -12,28 +12,29 @@ class CustProfileController extends Controller
         
         return view('/customer/custProfile', compact('countPet'));
     }
+   
+    
     function userprofileID($user_id){
         $userprofile_id = DB::table('user_accounts')->where('user_id','=','$user_id')->first();
         return view('customer/custProf',compact('userprofile_id'));
     }
-
     function saveUser(Request $request,$user_id){
-    $request->validate([
-        'user_name'=>'required',
-        'user_password'=>'required',
-        'user_mobile'=>'required',
-        'user_email'=>'required',
-        'userType_id'=>'required',
-    ]);
-    DB:table('user_accounts')->where ('user_id','=',$user_id)
-    ->update(array(
-        'user_name'=>$request->user_name,
-        'user_password'=>$request->user_password,
-        'user_mobile'=>$request->user_mobile,
-        'user_email'=>$request->user_email,
-        'userType_id'=>$request->userType_id,
-    ));
-    return redirect('customer/custProf')->with('success','User has been updated successfully');
-}
-
-}
+        $request->validate([
+            'user_name'=>'required',
+            'user_password'=>'required',
+            'user_mobile'=>'required',
+            'user_email'=>'required',
+            'userType_id'=>'required',
+        ]);
+        DB:table('user_accounts')->where ('user_id','=',$user_id)
+        ->update(array(
+            'user_name'=>$request->user_name,
+            'user_password'=>$request->user_password,
+            'user_mobile'=>$request->user_mobile,
+            'user_email'=>$request->user_email,
+            'userType_id'=>$request->userType_id,
+        ));
+        return redirect('custProf')->with('success','User has been updated successfully');
+    }
+    
+    }
