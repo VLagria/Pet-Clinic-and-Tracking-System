58422377b3a4cf6b5a7d61f027211fbb2fe62aea
hannahramirez15 <47436900+hannahramirez15@users.noreply.github.com>
2021-08-12 16:45:45 +0800

    test

--- app/Http/Controllers/VeterinariansController.php
+++ app/Http/Controllers/VeterinariansController.php
@@ -43,6 +43,7 @@ class VeterinariansController extends Controller
         'veterinary.vet_mobile','veterinary.vet_tel','veterinary.vet_birthday','veterinary.vet_DP', DB::raw("CONCAT(vet_blk, ' ', vet_street,' ',vet_subdivision,' ',vet_barangay,' ',
         vet_city,' ',vet_zip) AS vet_address"),'veterinary.vet_dateAdded','clinic.clinic_name','veterinary.vet_isActive')
         ->paginate(10);
+       
 
         return view('veterinary/viewvet', ['veterinaries'=>$veterinaries]);
     }
