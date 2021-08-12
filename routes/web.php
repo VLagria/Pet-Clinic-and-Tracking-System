a9165681ecbf58b819e3447b3fc797629a3ff111
Ericka Isleta <86280222+eisleta@users.noreply.github.com>
2021-08-13 01:05:23 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -409,5 +409,5 @@ Route::post('/customer/custhome', [Customercontroller ::class, 'getCustomerPet']
 
 Route::get('/customer/custAcc', [CustProfileController:: class, 'editProfile']);
 Route::get('/customer/custeditProfile', [CustProfileController:: class, 'userProfile']);
-Route::POST('/customer/custAcc/{customer_id}/{user_id}', [CustProfileController::class, 'saveProfile'])->name('save.custimage');
+Route::POST('/customer/custAcc/{customer_id}/{user_id}', [CustProfileController::class, 'custProfile'])->name('save.custimage');
 
